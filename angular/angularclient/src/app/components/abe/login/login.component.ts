1976fb7f300c1eee9ebee6bd2e189ca92e55f4b8
Abraham Pierrot <pierrota@merrimack.edu>
2019-10-25 10:09:06 -0400

    connect button with the appropriate page, fix background

--- angular/angularclient/src/app/components/abe/login/login.component.ts
+++ angular/angularclient/src/app/components/abe/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BackEndService } from 'src/app/services/backend';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,11 +9,19 @@ import { BackEndService } from 'src/app/services/backend';
 })
 export class LoginComponent implements OnInit {
   email:string;
-  passwordd:string;
+  password:string;
 
-  constructor() { }
+  constructor(private cache: BackEndService,private router: Router) { }
 
   validateData(){
+   this.cache.requestUserLogin(this.email,this.password);
+
+   this.router.navigate(['/', 'look']);
+   if(this.cache.userCanLogin){
+     console.log("user can log in");
+   }else{
+     console.log("user can't log in");
+   }
     //check if the information is right with the back end if so send user to store page
   }
 
--- angular/angularclient/src/app/components/start/start.component.ts
+++ angular/angularclient/src/app/components/start/start.component.ts
@@ -11,19 +11,19 @@ import {UserRegistrationComponent} from "../abe/user-registration/user-registrat
 export class StartComponent {
 
   constructor(private cache: BackEndService, private dialog: MatDialog) {
-    this.cache.requestChefs(); // use to get fresh list on construstion
-    this.cache.requestNewUser(this.cache.getBlankUser(2,"c","K",
-   "fkgfjhj","cki.ng@riverdale.edu","testme",""));
+  //   this.cache.requestChefs(); // use to get fresh list on construstion
+  //   this.cache.requestNewUser(this.cache.getBlankUser(2,"c","K",
+  //  "fkgfjhj","cki.ng@riverdale.edu","testme",""));
   }
 
-  test(){
-      // console.log(this.cache.getChefs());
-      this.cache.requestUser(2);
-      console.log(this.cache.user);
-      console.log(this.cache.userCanLogin);
-      this.cache.requestUserLogin(this.cache.user.email,"testme");
-      console.log(this.cache.userCanLogin);
-  }
+  // test(){
+  //     // console.log(this.cache.getChefs());
+  //     this.cache.requestUser(2);
+  //     console.log(this.cache.user);
+  //     console.log(this.cache.userCanLogin);
+  //     this.cache.requestUserLogin(this.cache.user.email,"testme");
+  //     console.log(this.cache.userCanLogin);
+  // }
 
   openLogin(){
       this.dialog.open(LoginComponent);
